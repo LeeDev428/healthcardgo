3c041bcb68221704ecfe28965e97126c62fd7fd0
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-23 23:39:47 +0800

    Update Homepage.php

--- app/Livewire/Home/Homepage.php
+++ app/Livewire/Home/Homepage.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Home;
 
+use App\Models\Announcement;
 use App\Services\DiseaseSurveillanceService;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\Layout;
@@ -21,6 +22,14 @@ class Homepage extends Component
     public function heatmapData(): array
     {
         return $this->surveillanceService->getHeatmapData(null, '30days');
+    }
+
+    #[Computed]
+    public function latestAnnouncement()
+    {
+        return Announcement::where('is_active', true)
+            ->latest()
+            ->first();
     }
 
     public function render()
