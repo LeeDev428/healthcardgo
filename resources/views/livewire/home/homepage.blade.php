f7bc729589293e0d9657b3a074915c73da2d8bd9
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-23 23:58:54 +0800

    Update homepage.blade.php

--- resources/views/livewire/home/homepage.blade.php
+++ resources/views/livewire/home/homepage.blade.php
@@ -1,25 +1,4 @@
 <div id="main">
-  <!-- Announcement Banner -->
-  @if($this->latestAnnouncement)
-    <div class="bg-blue-50 dark:bg-blue-900/20 border-l-4 border-blue-500 p-4 mb-6">
-      <div class="flex items-start">
-        <div class="flex-shrink-0">
-          <svg class="h-5 w-5 text-blue-500" fill="currentColor" viewBox="0 0 20 20">
-            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"/>
-          </svg>
-        </div>
-        <div class="ml-3 flex-1">
-          <h3 class="text-sm font-medium text-blue-800 dark:text-blue-200">
-            {{ $this->latestAnnouncement->title }}
-          </h3>
-          <div class="mt-2 text-sm text-blue-700 dark:text-blue-300">
-            <p>{{ $this->latestAnnouncement->content }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  @endif
-
   <x-homepage-header id="home">
     <div class="header__content">
       <h1 data-key="header_title">{{ __('app.header_title') }}</h1>
