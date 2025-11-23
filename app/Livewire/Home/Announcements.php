415c3ef250e87cb146bcc3936cda8fbb83629c65
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-23 23:58:46 +0800

    Update Announcements.php

--- app/Livewire/Home/Announcements.php
+++ app/Livewire/Home/Announcements.php
@@ -2,14 +2,23 @@
 
 namespace App\Livewire\Home;
 
+use App\Models\Announcement;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 #[Layout('components.layouts.home')]
 class Announcements extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.home.announcements');
+        return view('livewire.home.announcements', [
+            'announcements' => Announcement::where('is_active', true)
+                ->with('creator')
+                ->latest()
+                ->paginate(6),
+        ]);
     }
 }
